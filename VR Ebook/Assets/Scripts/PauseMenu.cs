26a13c8fcdcb734e9f0f110acbc19ee83f1ca390
ThithipongRuesupa <Rockqr.rocker@gmail.com>
2024-02-28 19:20:02 +0700

    Add PuaseMenu Script&UI

--- VR Ebook/Assets/Scripts/PauseMenu.cs
+++ VR Ebook/Assets/Scripts/PauseMenu.cs
@@ -1,18 +1,70 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject pauseMenuUI;
+    public bool GameIsPaused = true;
+    public InputActionAsset actionAsset;
+    private InputAction pauseAction;
+
     void Start()
     {
-        
+        DisplayWrisUI();
+    }
+    void Awake()
+    {
+        pauseAction = actionAsset.FindAction("Pause");
+        pauseAction.performed += PauseButtonPressed; 
+        pauseAction.Enable();
+    }
+    void OnDestroy()
+    {
+        pauseAction.Disable();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnPauseButtonPressed()
     {
-        
+        InputAction.CallbackContext context = new InputAction.CallbackContext();
+        PauseButtonPressed(context);
+    }
+    public void PauseButtonPressed(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            DisplayWrisUI();
+        }
+    }
+    public void DisplayWrisUI()
+    {
+        if (GameIsPaused)
+        {
+            pauseMenuUI.SetActive(false);
+            GameIsPaused = false;
+            Time.timeScale = 1f;
+        }
+        else if (!GameIsPaused)
+        {
+            pauseMenuUI.SetActive(true);
+            GameIsPaused = true;
+            Time.timeScale = 0f;
+        }
+    }
+    public void RestartButton()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+    public void BackToMainMenuButton()
+    {
+        SceneManager.LoadScene(0);
+    }
+    public void BackToCatalogButtonPressed()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void QuitButtonPressed()
+    {
+        Application.Quit();
     }
 }
